3047b462bf95c8741e7dab5e7ddec8d64211b025
Aviansh Ganpat Markad <100151747+AvinashMarkad@users.noreply.github.com>
2024-01-25 22:03:24 +0530

    Add files via upload

---
+++ New Folder/main.py
@@ -0,0 +1,19 @@
+# n=int(input())
+# res=[]
+# grade=[]
+# for i in range(n):
+#     name=input()
+#     mark=float(input())
+#     res.append( [name,mark])
+#     grade.append(mark) #calculation 2nd lowest
+# grade=sorted(set(grade)) #sorted unique
+# m=grade(1)
+# name=[]
+# for val in res:
+#     if m==val(1):
+#         name. append (val [0])
+# name. sort()
+
+'''for nm in name:
+    print(nm)'''
+print("123")
